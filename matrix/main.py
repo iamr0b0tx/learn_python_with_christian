b27da66fdfe45f6f591e8c96c63626767b54228b
Joshua Christian <joshua3christianx@gmail.com>
2019-09-18 15:15:16 +0100

    matrix updated

--- matrix/main.py
+++ matrix/main.py
@@ -9,12 +9,14 @@ A =>
 
 def main():
     A = Matrix([[1,2,3],[4,5,6],[7,8,9]])
-    B = Matrix([1,2,3])
-    C = Matrix([1, [2, 3]])
-    
+    # B = Matrix([1,2,3])
+    # C = Matrix([1, [2, 3]])
+    B = Matrix([[10, 2, 3], [4, 50, 6]])
+
+
     print(A)
     print(B)
-    print(C)
+    print(A.add(B))
 
 
 if __name__ == '__main__':
--- matrix/matrix.py
+++ matrix/matrix.py
@@ -3,8 +3,22 @@ from utils import *
 class Matrix:
     def __init__(self, li):
         self.matrix = li
-        raise
-            
+        if self.validate() == False:
+            raise ValueError('Invalid dimension!')
+    
+    def add(self, matrix_object):
+        '''adds two matrix and returns the sum'''
+        A = self.matrix
+        B = matrix_object.matrix
+
+        matrix_sum = [[0 for _ in x] for x in A] #array[3][3]
+        
+        for i in range(len(A)):
+            for j in range(len(A[i])):
+                matrix_sum[i][j] = A[i][j] + B[i][j]
+
+        return matrix_sum
+
     def validate(self):
         length_of_value = None
         for value in self.matrix:
@@ -12,8 +26,22 @@ class Matrix:
                 length_of_value = find_length(value)
             else:
                 if length_of_value != find_length(value):
-                    return False     
+                    return False 
         return True
 
     def __str__(self):
-        return f'{self.matrix}'
+        if find_length(self.matrix[0]) == 0:
+            return f'{self.matrix}'
+        
+        else:
+            rep = 'Matrix => [\n'
+            for a in self.matrix:
+                rep += ' ['
+                for b in a:
+                    rep += str(b)+ ' '
+                
+                rep = rep.strip()
+                rep += ']\n'
+            rep += ']\n'
+            
+            return rep
